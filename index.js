ff234f8aa485bb0b0e261f432909a141976c87dd
Revanth-guduru-balaji <gbrevanth159@gmail.com>
2023-05-19 09:42:48 -0400

    ok

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 /********* feature/CloseOrOpenLayout *********/
 
-const containerOne = document.querySelector(".container_1");
+const containerOne = document.querySelector(".Imagecontainer");
 const openLayout = document.querySelector("#openLayout");
-
 function onCloseLayout(flag) {
-  if (flag) {
-    containerOne.style.display = "none";
-    openLayout.style.display = "block";
-  } else {
-    containerOne.style.display = "block";
-    openLayout.style.display = "none";
+    if (flag) {
+      containerOne.style.display = "none";
+      openLayout.style.display = "block";
+      document.querySelector('.main').classList.add('full-width');
+    } else {
+      containerOne.style.display = "block";
+      openLayout.style.display = "none";
+      document.querySelector('.main').classList.remove('full-width');
+    }
   }
-}
+  
+
 
 /********** Modal **********/
 
